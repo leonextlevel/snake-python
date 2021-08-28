0bc8a86bb8d35f2a38f957fcfd08c58dc4e2bf04
LeoNextLevel <leandrolopessjc15@gmail.com>
2021-08-27 21:50:35 -0300

    Mecanicas de gerar comida, comer e aumentar a Snake

--- assets/entity.py
+++ assets/entity.py
@@ -1,9 +1,29 @@
 import pygame
+from random import randint
 
 
-class Snake:
-    COLOR = 0, 200, 0
+class Model:
     SIZE = 40
+
+
+class Food(Model):
+    COLOR = 100, 0, 0
+
+    def __init__(self, x, y) -> None:
+        self.body = pygame.Rect(x, y, self.SIZE, self.SIZE)
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.COLOR, self.body)
+
+    def reposition(self, surface):
+        x = randint(0, (surface.get_width() // self.SIZE) - 1) * self.SIZE
+        y = randint(0, (surface.get_height() // self.SIZE) - 1) * self.SIZE
+        self.body.x = x
+        self.body.y = y
+
+
+class Snake(Model):
+    COLOR = 0, 200, 0
     DIRECTIONS = {
         'left': (-1, 0),
         'right': (1, 0),
@@ -23,11 +43,17 @@ class Snake:
             pygame.Rect(self.x, self.y, self.SIZE, self.SIZE),
         ]
 
+    def increment(self):
+        self.body.append(self.body[-1].copy())
+
     def move(self):
         for index, part in reversed(list(enumerate(self.body[1:], 1))):
             part.x = self.body[index - 1].x
             part.y = self.body[index - 1].y
         self.body[0].move_ip(*[value * self.SIZE for value in self.DIRECTIONS[self.direction]])
+
+    def has_collision(self, obj):
+        return self.body[0].colliderect(obj)
 
     def key_handler(self):
         key = pygame.key.get_pressed()
@@ -39,7 +65,7 @@ class Snake:
             self.direction = 'up'
         if key[pygame.K_DOWN]:
             self.direction = 'down'
-    
+
     def draw(self, surface):
         for part in self.body:
             pygame.draw.rect(surface, self.COLOR, part)
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import assets
 import sys, pygame
 
 from pygame import Rect
 from pygame import draw
 from pygame.event import Event
 
-from assets.entity import Snake
+from assets.entity import Snake, Food
 
 
 def main():
@@ -14,13 +15,14 @@ def main():
     SCREEN = pygame.display.set_mode(SIZE)
     clock = pygame.time.Clock()
     snake = Snake()
-    forma = Rect(40, 40, 40, 40)
+    food = Food(320, 320)
     pygame.init()
 
     MOVE_EVENT = Event(666)
-    pygame.time.set_timer(MOVE_EVENT, 250)
+    pygame.time.set_timer(MOVE_EVENT, 100)
 
     while True:
+        has_collision = False
         SCREEN.fill(BLACK)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -28,6 +30,11 @@ def main():
                 sys.exit()
             if event == MOVE_EVENT:
                 snake.move()
+                has_collision = snake.has_collision(food.body)
+        if has_collision:
+            snake.increment()
+            food.reposition(SCREEN)
+        food.draw(SCREEN)
         snake.draw(SCREEN)
         snake.key_handler()
         pygame.display.flip()
